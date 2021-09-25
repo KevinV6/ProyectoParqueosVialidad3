1415fbc34a7c1d1af68e0a6e00f395bf96617346
Kevin <bck1009309est.univalle.edu>
2021-09-25 17:59:50 -0400

    Epic 7 Tarea 1

---
+++ ProyectoVialidadASP/ProyectoVialidadASP/JavaScript/JIndex.js
@@ -0,0 +1,22 @@
+﻿window.addEventListener('scroll', function () {
+	var header = document.querySelector('header');
+	header.classList.toggle('sticky', window.scrollY > 0);
+});
+function noscroll() {
+	window.scrollTo(0, 0);
+}
+var aux = 1;
+function toggleMenu() {
+	var menuToggle = document.querySelector('.toggle');
+	var menu = document.querySelector('.menu');
+	menuToggle.classList.toggle('activate');
+	menu.classList.toggle('activate');
+	if (aux == 1) {
+		window.addEventListener("scroll", noscroll);
+		aux = 2;
+	} else {
+		window.removeEventListener("scroll", noscroll);
+		aux = 1;
+	}
+	console.wri
+}
