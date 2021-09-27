3c30d0bd13284739cdecb0c33d31486ffca54de0
Daniel <dansan1234567@gmail.com>
2021-09-26 21:53:14 -0400

    Epic 1, Task 1, Valeria-Daniel Epic 5, Task 3, Daniel

---
+++ proyectoparqueovialidad/lib/Register Complete/registerComplete.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:proyectovialidad/Register%20Complete/body.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Registro Completado',
+      theme: ThemeData(
+        primaryColor: Color(0xFF212871),
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: RegisterComplete(),
+    );
+  }
+}
+
+class RegisterComplete extends StatelessWidget {
+  const RegisterComplete({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Body(),);
+  }
+}
