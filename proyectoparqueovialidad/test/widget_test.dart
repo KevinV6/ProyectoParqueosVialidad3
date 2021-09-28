753ce5f43b0bd6251c65a871eda9790ca68dc1d4
onlyTheF <73356149+onlyTheF@users.noreply.github.com>
2021-09-27 20:46:55 -0400

    Solo update pero no final

--- proyectoparqueovialidad/test/widget_test.dart
+++ proyectoparqueovialidad/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:proyectoparqueovialidad/main.dart';
+import 'package:proyectoparqueovialidad/Epic5Task1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
