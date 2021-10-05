6116d9f26e29372af6e98f750275a3cb24daba4c
Nikypm <pmn0028257@est.univalle.edu>
2021-10-05 18:52:54 -0400

    Epic 12: Task1 y Task3

--- ProyectoVialidadASP/ProyectoVialidadASP/Controllers/LoginController.cs
+++ ProyectoVialidadASP/ProyectoVialidadASP/Controllers/LoginController.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ProyectoVialidadASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
-using System.Text.RegularExpressions;
 
 namespace ProyectoVialidadASP.Controllers
 {
     public class LoginController : Controller
     {
+        //Conexion a Firebase
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "F23JUcUtvSmaKQAZh1ZlzSEPL4WQ4wQirbrl3xpT",
+            BasePath = "https://proyectovialidadasp-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+
+
         // GET: Login
         public ActionResult Login()
         {
@@ -18,10 +33,43 @@ namespace ProyectoVialidadASP.Controllers
         [HttpPost]
         public ActionResult Validar(FormCollection datos)
         {
+            bool verificado = false;
             Regex r = new Regex("^[a-zA-Z0-9]+$");
             if (r.IsMatch(datos["userText"]) && r.IsMatch(datos["passText"]))
             {
-                return View("../Maps/Maps");
+                client = new FireSharp.FirebaseClient(config);
+                FirebaseResponse response = client.Get("Administrators");
+                dynamic data = JsonConvert.DeserializeObject<dynamic>(response.Body);
+                var list = new List<Administrator>();
+                foreach (var item in data)
+                {
+                    list.Add(JsonConvert.DeserializeObject<Administrator>(((JProperty)item).Value.ToString()));
+                }
+
+                foreach (var item in list)
+                {
+                    string password = string.Empty;
+                    byte[] decryted = Convert.FromBase64String(item.password);
+                    password = System.Text.Encoding.Unicode.GetString(decryted);
+                    if (item.userName.Equals(datos["userText"]) && password.Equals(datos["passText"]))
+                    {
+                        verificado = true;
+                    }
+                }
+
+                if (verificado)
+                {
+                    Session["user"] = datos["userText"];
+                    Session["psw"] = datos["passText"];
+                    return View("../Maps/Maps");
+                }
+
+                else
+                {
+                    ViewBag.MessagePass = "Nombre de Usuario o Contraseña incorrectos";
+                    return View("Login");
+                }
+
             }
             else
             {
@@ -35,9 +83,9 @@ namespace ProyectoVialidadASP.Controllers
                     ViewBag.MessageUser = "No usar Caracteres especiales en la contraseña ,";
                     return View("Login");
                 }
-                
+
             }
-            
+
         }
     }
 }
---
+++ ProyectoVialidadASP/ProyectoVialidadASP/Models/Administrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoVialidadASP.Models
+{
+    public class Administrator
+    {
+        #region Atributos
+        public string userName { get; set; }
+        public string password { get; set; }
+        #endregion
+
+
+        #region Constructor
+        public Administrator(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+        #endregion
+    }
+}
