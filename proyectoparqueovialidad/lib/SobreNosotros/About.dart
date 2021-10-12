4a3edfdf8b2d9ce50c5c6105cadec901afcdca4d
Bryan <Ggb0026564@est.univalle.edu>
2021-10-11 23:36:41 -0400

    Epic24, Task3 BryanG,Modificacion SobreNosotros

--- proyectoparqueovialidad/lib/SobreNosotros/About.dart
+++ proyectoparqueovialidad/lib/SobreNosotros/About.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:proyectoparqueovialidad/main.dart';
 
-void main() => runApp(const MyApp());
 
-/// This is the main application widget.
+void main() async {
+  runApp(MyApp());
+}
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
+    return  MaterialApp(
+      title: "Hola sobre",
       home: MyStatefulWidget(),
     );
   }
@@ -26,63 +29,211 @@ class MyStatefulWidget extends StatefulWidget {
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
+
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController controllerUser = new TextEditingController();
-  TextEditingController controllerPassword = new TextEditingController();
-  int _count = 0;
+
+  _launchURL(String value) async {
+    const url = 'https://www.facebook.com/gamcochabamba';
+    const url2 = 'https://www.cochabamba.bo/';
+    const url3 = 'https://twitter.com/gam_cochabamba';
+
+    if (value == 'facebook') {
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    } else if (value == 'pagina') {
+      if (await canLaunch(url2)) {
+        await launch(url2);
+      } else {
+        throw 'Could not launch $url2';
+      }
+    } else if (value == 'twitter') {
+      if (await canLaunch(url3)) {
+        await launch(url3);
+      } else {
+        throw 'Could not launch $url3';
+      }
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sobre nosotros', style: TextStyle(color: Colors.yellow),),
-        backgroundColor: Colors.indigo,
-
-      ),
-      body: Form(
-          child: Container(
-            child: Column(children: <Widget> [Container(
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(top: 93),
-              child: Column(
-                children: [
-                  Container(
-                    padding: EdgeInsets.only(
-                        top: 0, left: 20, right: 400, bottom: 0),
-                    child: Text("Esta Aplicacion esta hecha con la finalidad de saber"
-                        " que calles y parqueos estan disponibles, ya que la "
-                        " aplicacion cuenta con varias opciones que le permitira "
-                        " usar un mapa que le mostrara toda la ciudad y podra "
-                        " ver mas facil mente que parqueos estan disponibles "
-                        " o que calles estan bloqueadas, esta aplicacion "
-                        " tambien cuenta con un sistema de notificaciones que"
-                        " avisa que parqueos ya no estan disponibles y si quiere"
-                        " tener mas opciones puede registrarse en la aplicacion."),),
-
-                  Spacer( flex: 1,),
-                  Container(
-                    padding: EdgeInsets.only(
-                        top: 0, left: 0, right: 550, bottom: 0),
-                    child: Text("Para mas informacion revise nuestras redes sociales :"),),
-                  Spacer( flex: 1,),
-                  ButtonTheme(
-                    minWidth: 200.0,
-                    height: 50.0,
-                    buttonColor: Colors.indigo,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      textColor: Colors.amber,
-                      child: Text("Volver"),
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Card Drop',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
-            )
-            ],
-            ),)
+                ),
+                Text(
+                  'Alcaldia Cochabamba',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.airport_shuttle_rounded,
+            color: Colors.red[500],
+          ),
+
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget textSection = Container(
+      padding: EdgeInsets.only( top: 0, left: 15, right: 2, bottom: 0),
+      child: Text("Esta Aplicacion esta hecha con la finalidad de saber"
+          " que calles y parqueos estan disponibles, ya que la "
+          " aplicacion cuenta con varias opciones que le permitira "
+          " usar un mapa que le mostrara toda la ciudad y podra "
+          " ver mas facil mente que parqueos estan disponibles "
+          " o que calles estan bloqueadas, esta aplicacion "
+          " tambien cuenta con un sistema de notificaciones que"
+          " avisa que parqueos ya no estan disponibles y si quiere"
+          " tener mas opciones puede registrarse en la aplicacion."
+      ),
+    );
+    Widget textSection1 = Container(
+      padding: EdgeInsets.only( top: 0, left: 15, right: 2, bottom: 0),
+      child: Text(""),
+    );
+    Widget textSection2 = Container(
+      padding: EdgeInsets.only( top: 0, left: 15, right: 2, bottom: 0),
+      child: Text("Para mas informacion revise nuestras redes sociales :"
+      ),
+    );
+    Widget textSection3 = Container(
+      padding: EdgeInsets.only( top: 0, left: 2, right: 2, bottom: 0),
+      child: Text(""),
+    );
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget> [
+          IconButton( icon: Image.asset("assets/image/SocialButton/facebook.png"), iconSize: 55,
+            onPressed: (){
+              _launchURL('facebook');
+            },
+          ),
+          IconButton( icon: Image.asset("assets/image/SocialButton/pagina.png"),iconSize: 61,
+            onPressed: (){
+              _launchURL('pagina');
+            },
+          ),
+          IconButton( icon: Image.asset("assets/image/SocialButton/twitter.png"),iconSize: 55,
+            onPressed: ()  {
+              _launchURL('twitter');
+            },
+          )
+        ],
+      ),
+    );
+
+    Widget buttonSectio2 = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonTheme(
+            minWidth: 50.0,
+            height: 30.0,
+            buttonColor: Colors.blueGrey,
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, MyHomePage.id);
+              },
+              textColor: Colors.amber,
+              child: Text("Volver"),
+            ),
+          ),
+        ],
+      ),
+
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sobre nosotros',textAlign: TextAlign.center,style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white,
+
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/image/SobreNosotros/parqueo.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            textSection,
+            textSection1,
+            textSection2,
+            textSection3,
+            buttonSection,
+            buttonSectio2,
+
+          ],
+        ),
       ),
     );
   }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+
+
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
