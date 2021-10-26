461af6e0837269345dcc181ad1478a7cecb49796
Jose <BTJ0027390@est.univalle.edu>
2021-10-26 00:15:51 -0400

    Alerta de estados de calles no disponibles y parqueos

---
+++ ProyectoVialidadASP/ProyectoVialidadASP/Js/JAlertStreet.js
@@ -0,0 +1,20 @@
+﻿function CheckConfirmDelet() {
+
+    if (confirm('Estas seguro de que quieres eliminar?'))
+        __doPostBack('BtnDeleteSelected', '');
+    else
+        return false;
+
+    return true;
+
+}
+function CheckConfirmActive() {
+
+    if (confirm('Estas seguro de que quieres activar?'))
+        __doPostBack('BtnDeleteSelected', '');
+    else
+        return false;
+
+    return true;
+
+}
